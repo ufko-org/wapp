735b773e079d201c583558c21320d65955f8cbe2
Ufko <ufko.org@proton.me>
2025-02-16 08:23:13 +0100

    Initial commit

---
+++ app.tcl
@@ -0,0 +1,19 @@
+# -- controllers
+#proc wapp-default {} {
+#	wapp-trim {
+#		hello
+#	}
+#}
+
+#proc wapp-page-env {} {
+#	wapp-trim {
+#		wapp-debug-env
+#	}
+#}
+
+# -- helpers
+#proc hello {} {
+#	hello
+#}
+
+# -- etc ...
---
+++ wapptclsh.tcl
@@ -0,0 +1,33 @@
+# This script runs to initialize wapptclsh.
+#
+proc initialize_wapptclsh {} {
+  global argv argv0 main_script
+  if {[llength $argv]==0} {
+		puts stdout "Wapp 1.0 shell using SQLite version [sqlite3 -version]\
+								 and TCL $::tcl_version"
+		return
+  } else {
+    set script [lindex $argv 0]
+  }
+  if {[string index $script 0]=="-"} {
+    set opt [string trim $script -]
+    if {$opt=="v"} {
+      puts stderr "Wapp using SQLite version [sqlite3 -version]\
+                   and TCL $::tcl_version"
+    } else {
+      puts stderr "Usage: $argv0 FILENAME"
+      puts stderr "Options:"
+      puts stderr "   -v      Show version information"
+    }
+    exit 1
+  } elseif {[file readable $script]} {
+    set fd [open $script r]
+    set main_script [read $fd]
+    close $fd
+    set argv [lrange $argv 1 end]
+    set argv0 $script
+  } else {
+    puts stderr "unknown option: \"$script\"\nthe --help option is available"
+  }
+}
+initialize_wapptclsh

ca0076a67ba246679e521e3995e72504a65e02a7
Ufko <ufko.org@proton.me>
2025-02-22 18:30:49 +0100

    Clean-Fu

--- wapptclsh.tcl
+++ wapptclsh.tcl
@@ -1,7 +1,9 @@
 # This script runs to initialize wapptclsh.
 #
-proc initialize_wapptclsh {} {
+proc wapptclsh_init {} {
   global argv argv0 main_script
+	set wapp_version "1.0"
+
   if {[llength $argv]==0} {
 		if {[info commands main] eq "main"} {
 			main
@@ -14,31 +16,40 @@ proc initialize_wapptclsh {} {
 |_____|__,|  _|  _|
           |_| |_|  
 "
-		puts "Wapp 1.0 shell using SQLite version [sqlite3 -version]\
+		puts "Wapp $wapp_version shell using SQLite version [sqlite3 -version]\
 				  and TCL $::tcl_version"
+		puts " "
 		return
   } else {
     set script [lindex $argv 0]
-  }
-  if {[string index $script 0]=="-"} {
-    set opt [string trim $script -]
-    if {$opt=="v"} {
-      puts stderr "Wapp using SQLite version [sqlite3 -version]\
-                   and TCL $::tcl_version"
-    } else {
-      puts stderr "Usage: $argv0 FILENAME"
-      puts stderr "Options:"
-      puts stderr "   -v      Show version information"
-    }
-    exit 1
-  } elseif {[file readable $script]} {
-    set fd [open $script r]
-    set main_script [read $fd]
-    close $fd
-    set argv [lrange $argv 1 end]
-    set argv0 $script
-  } else {
-    puts stderr "unknown option: \"$script\"\nthe --help option is available"
-  }
+  	if {[string index $script 0]=="-"} {
+    	set opt [string trim $script -]
+   	 	if {$opt=="v" || $opt=="version"} {
+      	puts stderr "Wapp $wapp_version using SQLite version [sqlite3 -version]\
+                     and TCL $::tcl_version"
+			} elseif {$opt=="h" || $opt=="help"} {
+				puts stderr "Usage: $argv0 FILENAME \[--server port\]"
+        puts stderr "Options:"
+        puts stderr "   -v --version        Show version information"
+        puts stderr "   -h --help           Show this help message"
+        puts stderr "      --server port    Start the server on the given port"
+        puts stderr "If no arguments are provided, an interactive shell is started."
+				puts stderr "See also project homepage: https://github.com/ufko-org/wapp"
+    	} else {
+				puts "Unknown option: -$opt, use -h --help for help"
+    	}
+    	exit 1
+  	} elseif {[file readable $script]} {
+    	set fd [open $script r]
+    	set main_script [read $fd]
+    	close $fd
+    	set argv [lrange $argv 1 end]
+    	set argv0 $script
+  	} else {
+			puts stderr "Error: File \"$script\" does not exist or has incorrect permissions."
+			exit 
+  	}
+	}
 }
-initialize_wapptclsh
+
+wapptclsh_init
